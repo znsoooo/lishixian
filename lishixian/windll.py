2b429d79e561cd65a0d7bbeaa38e01134200c356
lsx <11313213@qq.com>
2022-02-27 02:03:04 +0800

    Remove `lpstrDefExt` seem like useless

--- lishixian/windll.py
+++ lishixian/windll.py
@@ -1,7 +1,7 @@
 """Windows Dialog"""
 
 import ctypes
-import ctypes.wintypes as wintypes
+from ctypes.wintypes import DWORD, WORD, LPARAM, LPVOID, HWND, HINSTANCE
 
 LPOFNHOOKPROC = ctypes.c_void_p
 LPCTSTR = LPTSTR = ctypes.c_wchar_p
@@ -17,49 +17,48 @@ GetSaveFileName = ctypes.windll.comdlg32.GetSaveFileNameW
 
 
 class OPENFILENAME(ctypes.Structure):
-    _fields_ = [('lStructSize',       wintypes.DWORD),
-                ('hwndOwner',         wintypes.HWND),
-                ('hInstance',         wintypes.HINSTANCE),
+    _fields_ = [('lStructSize',       DWORD),
+                ('hwndOwner',         HWND),
+                ('hInstance',         HINSTANCE),
                 ('lpstrFilter',       LPCTSTR),
                 ('lpstrCustomFilter', LPTSTR),
-                ('nMaxCustFilter',    wintypes.DWORD),
-                ('nFilterIndex',      wintypes.DWORD),
+                ('nMaxCustFilter',    DWORD),
+                ('nFilterIndex',      DWORD),
                 ('lpstrFile',         LPTSTR),
-                ('nMaxFile',          wintypes.DWORD),
+                ('nMaxFile',          DWORD),
                 ('lpstrFileTitle',    LPTSTR),
-                ('nMaxFileTitle',     wintypes.DWORD),
+                ('nMaxFileTitle',     DWORD),
                 ('lpstrInitialDir',   LPCTSTR),
                 ('lpstrTitle',        LPCTSTR),
-                ('flags',             wintypes.DWORD),
-                ('nFileOffset',       wintypes.WORD),
-                ('nFileExtension',    wintypes.WORD),
+                ('flags',             DWORD),
+                ('nFileOffset',       WORD),
+                ('nFileExtension',    WORD),
                 ('lpstrDefExt',       LPCTSTR),
-                ('lCustData',         wintypes.LPARAM),
+                ('lCustData',         LPARAM),
                 ('lpfnHook',          LPOFNHOOKPROC),
                 ('lpTemplateName',    LPCTSTR),
-                ('pvReserved',        wintypes.LPVOID),
-                ('dwReserved',        wintypes.DWORD),
-                ('flagsEx',           wintypes.DWORD)]
+                ('pvReserved',        LPVOID),
+                ('dwReserved',        DWORD),
+                ('flagsEx',           DWORD)]
 
 
-def BuildOFN(title, defext, filter, buf):
+def BuildOFN(title, filter, buf):
     ofn = OPENFILENAME()
     ofn.lStructSize = ctypes.sizeof(OPENFILENAME)
     ofn.lpstrTitle = title
     ofn.lpstrFile = ctypes.cast(buf, LPTSTR)
     ofn.nMaxFile = MAX_PATH
-    ofn.lpstrDefExt = defext
     ofn.lpstrFilter = filter # 'All types (*.*)\0*.*\0'
     ofn.Flags = OFN_ENABLESIZING | OFN_PATHMUSTEXIST | OFN_OVERWRITEPROMPT | OFN_NOCHANGEDIR
     return ofn
 
 
-def FileDialog(call, title, defext, filter, path):
+def FileDialog(call, title, filter, path):
     filter = filter.replace('|', '\0')
     if filter and not filter.endswith('\0'):
         filter += '\0'
     buf = ctypes.create_unicode_buffer(path, MAX_PATH)
-    ofn = BuildOFN(title, defext, filter, buf)
+    ofn = BuildOFN(title, filter, buf)
     if call(ctypes.byref(ofn)):
         return buf[:].split('\0', 1)[0]
 
@@ -70,12 +69,12 @@ __all__ = list(globals())
 messagebox = lambda info, title='Message', style=0: ctypes.windll.user32.MessageBoxW(0, str(info), str(title), style)
 
 
-def OpenFileDialog(title=None, defext='', filter='', path=''):
-    return FileDialog(GetOpenFileName, title, defext, filter, path)
+def OpenFileDialog(title=None, filter='', path=''):
+    return FileDialog(GetOpenFileName, title, filter, path)
 
 
-def SaveFileDialog(title=None, defext='', filter='', path=''):
-    return FileDialog(GetSaveFileName, title, defext, filter, path)
+def SaveFileDialog(title=None, filter='', path=''):
+    return FileDialog(GetSaveFileName, title, filter, path)
 
 
 __all__ = [k for k in globals() if k not in __all__]

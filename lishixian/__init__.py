54f08e79be16c21fd0d396862ffd2ebed4e783d0
lsx <11313213@qq.com>
2022-05-01 22:33:42 +0800

    Add: Pretty printer `help` function

--- lishixian/__init__.py
+++ lishixian/__init__.py
@@ -3,6 +3,25 @@ _print = print
 
 __keep__ = list(globals()) # Don't use word `__all__`, its will rewrite many times below.
 
+
+def help():
+    import inspect
+    import importlib
+    print("\n# Lishixian Library")
+    print("Contain %d functions." % len(__all__))
+    print("\n## Top module")
+    for k, v in globals().items():
+        if not k.startswith('_'):
+            if inspect.ismodule(v):
+                print("\n## Module '%s'" % k)
+            else:
+                try:
+                    argspec = str(inspect.signature(v))
+                    print("- %s%s" % (k, argspec))
+                except (TypeError, ValueError) as e:
+                    print("- %s = %r" % (k, v))
+
+
 # base on built-in library
 from .cls import *
 from .line import *
@@ -17,14 +36,5 @@ from .doc import *
 from .gui import *
 from .auto import *
 
-
-def help():
-    for k in __all__:
-        if not k.startswith('_'):
-            _help(k)
-
-
 # __all__ = [k for k in globals() if k not in __keep__]
 __all__ = [k for k, v in globals().items() if k not in __keep__ and callable(v)]
-
-_print('functions:', len(__all__))

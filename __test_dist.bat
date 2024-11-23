1125d47d9b987ea2e9eda2208111d20970273d79
Shixian Li <lsx7@sina.com>
2024-11-23 17:12:37 +0800

    Chore: Clean last package before building

--- __test_dist.bat
+++ __test_dist.bat
@@ -7,17 +7,19 @@ for /f "tokens=2 delims='" %%i in (lishixian/version.py) do (set ver=%%i)
 echo Version: %ver%
 echo.
 
-rename lsx lishixian
+rename lsx lishixian 1>nul 2>nul
+del dist\lishixian-%ver%.tar.gz 1>nul 2>nul
 %python% setup.py sdist
 echo.
 %python% -m pip install dist/lishixian-%ver%.tar.gz
 echo.
 
-rename lishixian lsx
+rename lishixian lsx 1>nul 2>nul
+del dist\lsx-%ver%.tar.gz 1>nul 2>nul
 %python% setup.py sdist
 echo.
 %python% -m pip install dist/lsx-%ver%.tar.gz
 echo.
 
-rename lsx lishixian
+rename lsx lishixian 1>nul 2>nul
 pause
